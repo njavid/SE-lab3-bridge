d3c389db377765f0a77be70600dd51ced66d0a6b
Ehyaee <z.ehyaee96@gmail.com>
2021-04-08 17:32:13 +0430

    Compelete implementation

--- src/com/company/MultipleImpRecursive.java
+++ src/com/company/MultipleImpRecursive.java
@@ -3,6 +3,11 @@ package com.company;
 public class MultipleImpRecursive extends MultipleImp{
     @Override
     public int implement(int x, int y) {
-        return 0;
+        if (y == 1){
+            return x;
+        }
+        else {
+            return x + implement(x, y-1);
+        }
     }
 }

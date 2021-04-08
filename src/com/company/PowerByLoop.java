69a21c2f5b4e77d740ab8152cc0c8fc98fd11ab5
Ehyaee <z.ehyaee96@gmail.com>
2021-04-08 18:29:01 +0430

    Add tests

---
+++ src/com/company/PowerByLoop.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class PowerByLoop extends PowerImp{
+    PowerByLoop(MultipleImp multipleImp) {
+        super(multipleImp);
+    }
+
+    @Override
+    public int implement(int x, int y) {
+        return 0;
+    }
+}
+
--- src/com/company/PowerImp.java
+++ src/com/company/PowerImp.java
@@ -1,3 +1,13 @@
 package com.company;
 
-public abstract class PowerImp implements Imp{}
+public abstract class PowerImp implements Imp{
+    public MultipleImp multipleImp;
+
+    PowerImp(MultipleImp multipleImp){
+        this.multipleImp = multipleImp;
+    }
+
+    public void setMultipleImp(MultipleImp multipleImp) {
+        this.multipleImp = multipleImp;
+    }
+}

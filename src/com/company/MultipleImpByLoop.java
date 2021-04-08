5ff538cc09dbc1c9288a6dac7e49449b05ee4061
Ehyaee <z.ehyaee96@gmail.com>
2021-04-08 17:15:58 +0430

    Compelete implementation

--- src/com/company/MultipleImpByLoop.java
+++ src/com/company/MultipleImpByLoop.java
@@ -3,6 +3,10 @@ package com.company;
 public class MultipleImpByLoop extends MultipleImp {
     @Override
     public int implement(int x, int y) {
-        return 0;
+        int result = 0;
+        for (int i = 0 ; i < y ; i++ ){
+            result += x;
+        }
+        return result;
     }
 }
